49955273a2214538dafea87bae4cf46f3c531a30
Илья Ефимов <inferrna@gmail.com>
2019-05-10 21:06:26 +0800

    Benchmark for get

--- src/bencher.rs
+++ src/bencher.rs
@@ -5,6 +5,7 @@ use criterion::Criterion;
 use criterion::black_box;
 use self::rand::Rng;
 use intmap::{IntMap,Keytype};
+use std::sync::Arc;
 
 fn many_put(cnt: usize, stride: usize) {
     let mut hm = IntMap::<String>::new(stride);
@@ -16,11 +17,33 @@ fn many_put(cnt: usize, stride: usize) {
     }
 }
 
-fn bench_put(c: &mut Criterion) {
-    for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
-        c.bench_function(format!("put {} {}", cnt, stride).as_mut_str(), move |b| b.iter(|| many_put(black_box(cnt), black_box(stride))));
+fn many_get(hm: Arc<IntMap<String>>, cnt: usize, stride: usize) {
+    let mut rng = rand::thread_rng();
+    for _ in 0..cnt {
+        let k: Keytype = rng.gen_range(0, cnt);
+        hm.get(k);
     }
 }
 
-criterion_group!(benches, bench_put);
+fn bench_put(c: &mut Criterion) {
+    for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
+        c.bench_function(format!("put {} values to {} chained map", cnt, stride).as_mut_str(), move |b| b.iter(|| many_put(black_box(cnt), black_box(stride))));
+    }
+}
+
+fn bench_get(c: &mut Criterion) {
+    for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
+        let mut hm = IntMap::<String>::new(stride);
+        let mut rng = rand::thread_rng();
+        for _ in 0..cnt {
+            let k: Keytype = rng.gen();
+            let s = format!("Street - {}", k);
+            hm.put(k, s);
+        }
+        let hma = Arc::new(hm);
+        c.bench_function(format!("get from {} chained map filled with {} values", stride, cnt).as_mut_str(), move |b| b.iter(|| many_get(hma.clone(),black_box(cnt), black_box(stride))));
+    }
+}
+
+criterion_group!(benches, bench_put, bench_get);
 criterion_main!(benches);
