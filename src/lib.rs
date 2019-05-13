cd95eb9574b006195c03e2e82c10ad8ba7f8fc69
Илья Ефимов <inferrna@gmail.com>
2019-05-13 16:00:17 +0800

    Tried wrap with mutex on master

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use std::ops::Deref;
+use std::sync::{Mutex, Arc};
+use core::borrow::Borrow;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 pub type Keytype = usize;
 
@@ -14,17 +18,16 @@ impl<T> Entry<T> {
     }
 }
 
-pub struct IntMap<T> {
+pub struct _IntMap<T> {
     /* Double hashing implemented with nested vectors */
     entries: Vec<Vec<Option<Entry<T>>>>,
     stride: Keytype,
 }
 
-impl<T> IntMap<T> {
-    pub fn new (stride: Keytype) -> IntMap<T> {
-
-        IntMap { entries: (0..stride).map(|_| vec![]).collect(),
-                  stride: stride}
+impl<T> _IntMap<T> {
+    pub fn new (stride: Keytype) -> _IntMap<T> {
+        _IntMap { entries: (0..stride).map(|_| vec![]).collect(),
+                   stride: stride}
     }
     fn find_pos (&self, key: Keytype) -> Option<usize> {
         /* Remainder as secondary hash value */
@@ -81,13 +84,44 @@ impl<T> IntMap<T> {
 }
 
 /* contains_value implemented only for PartialEq capable types */
-impl<T: PartialEq> IntMap<T> {
+impl<T: PartialEq> _IntMap<T> {
     pub fn contains_value(&self, value: T) -> bool {
         let res = self.entries.iter().flatten().filter_map(|e| e.as_ref()).find(|e| e.value == value);
         match res {
             Some(_) => true,
             None => false
         }
+    }
+}
+
+pub struct IntMap<T> {
+    real_map: Arc<Mutex<_IntMap<T>>>
+}
+
+impl<T> IntMap<T> {
+    pub fn new (stride: Keytype) -> IntMap<T> {
+        let map = _IntMap::<T>::new(stride);
+        IntMap { real_map: Arc::new(Mutex::new(map)) }
+    }
+    pub fn put(&mut self, key: Keytype, value: T) {
+        self.real_map.lock().unwrap().put(key, value)
+    }
+    pub fn contains_key(&mut self, key: Keytype) -> bool {
+        self.real_map.lock().unwrap().contains_key(key)
+    }
+    pub fn get(&self, key: Keytype) -> Option<&T> {
+        let map = self.real_map.lock().unwrap();
+        let res = map.get(key);
+        return res;
+    }
+    pub fn remove(&mut self, key: Keytype) -> Option<T> {
+        self.real_map.lock().unwrap().remove(key)
+    }
+}
+
+impl<T: PartialEq> IntMap<T> {
+    pub fn contains_value(&self, value: T) -> bool {
+        self.real_map.lock().unwrap().contains_value(value)
     }
 }
 
