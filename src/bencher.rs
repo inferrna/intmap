3eac2a4c6f8b9e4248df08d8839fbd51d15333fb
Илья Ефимов <inferrna@gmail.com>
2019-05-13 20:58:19 +0800

    i32 benches

--- src/bencher.rs
+++ src/bencher.rs
@@ -7,7 +7,7 @@ use self::rand::Rng;
 use intmap::{IntMap,Keytype};
 use std::sync::Arc;
 
-fn many_put(cnt: usize, stride: usize) {
+fn many_put_string(cnt: usize, stride: usize) {
     let mut hm = IntMap::<String>::new(stride);
     let mut rng = rand::thread_rng();
     for _ in 0..cnt {
@@ -17,11 +17,20 @@ fn many_put(cnt: usize, stride: usize) {
     }
 }
 
-fn many_new(stride: usize) {
-    let hm = black_box(IntMap::<String>::new(stride));
+fn many_put_i32(cnt: usize, stride: usize) {
+    let mut hm = IntMap::<i32>::new(stride);
+    let mut rng = rand::thread_rng();
+    for i in 0..cnt {
+        let k: Keytype = rng.gen();
+        hm.put(k, i as i32);
+    }
 }
 
-fn many_get(hm: Arc<IntMap<String>>, cnt: usize ) {
+fn many_new<T>(stride: usize) {
+    let hm = black_box(IntMap::<T>::new(stride));
+}
+
+fn many_get<T: Clone>(hm: Arc<IntMap<T>>, cnt: usize ) {
     let mut rng = rand::thread_rng();
     for _ in 0..cnt {
         let k: Keytype = rng.gen_range(0, cnt);
@@ -29,19 +38,41 @@ fn many_get(hm: Arc<IntMap<String>>, cnt: usize ) {
     }
 }
 
-fn bench_put(c: &mut Criterion) {
+fn bench_put_i32(c: &mut Criterion) {
     for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
-        c.bench_function(format!("put {} values to {} chained map", cnt, stride).as_mut_str(), move |b| b.iter(|| many_put(black_box(cnt), black_box(stride))));
+        c.bench_function(format!("i32 put {} values to {} chained map", cnt, stride).as_mut_str(), move |b| b.iter(|| many_put_i32(black_box(cnt), black_box(stride))));
+    }
+}
+fn bench_put_string(c: &mut Criterion) {
+    for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
+        c.bench_function(format!("str put {} values to {} chained map", cnt, stride).as_mut_str(), move |b| b.iter(|| many_put_string(black_box(cnt), black_box(stride))));
     }
 }
 
-fn bench_new(c: &mut Criterion) {
+fn bench_new_i32(c: &mut Criterion) {
     for stride in vec![2000usize, 1000,  200] {
-        c.bench_function(format!("create {} chained map", stride).as_mut_str(), move |b| b.iter(|| many_new(black_box(stride))));
+        c.bench_function(format!("i32 create {} chained map", stride).as_mut_str(), move |b| b.iter(|| many_new::<String>(black_box(stride))));
+    }
+}
+fn bench_new_string(c: &mut Criterion) {
+    for stride in vec![2000usize, 1000,  200] {
+        c.bench_function(format!("str create {} chained map", stride).as_mut_str(), move |b| b.iter(|| many_new::<i32>(black_box(stride))));
     }
 }
 
-fn bench_get(c: &mut Criterion) {
+fn bench_get_i32(c: &mut Criterion) {
+    for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
+        let mut hm = IntMap::<i32>::new(stride);
+        let mut rng = rand::thread_rng();
+        for i in 0..cnt {
+            let k: Keytype = rng.gen();
+            hm.put(k, i as i32);
+        }
+        let hma = Arc::new(hm);
+        c.bench_function(format!("i32 get from {} chained map filled with {} values", stride, cnt).as_mut_str(), move |b| b.iter(|| many_get::<i32>(hma.clone(),black_box(cnt))));
+    }
+}
+fn bench_get_string(c: &mut Criterion) {
     for (cnt, stride) in vec![(100000usize, 2000usize), (100000, 1000), (100000, 200), (10000, 2000), (10000, 1000), (10000, 200)] {
         let mut hm = IntMap::<String>::new(stride);
         let mut rng = rand::thread_rng();
@@ -51,9 +82,10 @@ fn bench_get(c: &mut Criterion) {
             hm.put(k, s);
         }
         let hma = Arc::new(hm);
-        c.bench_function(format!("get from {} chained map filled with {} values", stride, cnt).as_mut_str(), move |b| b.iter(|| many_get(hma.clone(),black_box(cnt))));
+        c.bench_function(format!("str get from {} chained map filled with {} values", stride, cnt).as_mut_str(), move |b| b.iter(|| many_get::<String>(hma.clone(),black_box(cnt))));
     }
 }
 
-criterion_group!(benches, bench_put, bench_get, bench_new);
+
+criterion_group!(benches, bench_new_i32, bench_new_string, bench_put_i32, bench_put_string, bench_get_i32, bench_get_string);
 criterion_main!(benches);
